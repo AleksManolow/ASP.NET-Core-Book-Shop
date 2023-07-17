9336fc2d6c1e8e0dc5d5e8c5fdf09e69245370b8
AleksManolow <amanolov7@gmail.com>
2023-07-17 16:29:46 +0300

    Creaing bookfrom ratial view and add book in BookController

--- BookShopSystem.Web/Controllers/BookController.cs
+++ BookShopSystem.Web/Controllers/BookController.cs
@@ -1,6 +1,10 @@
 ﻿using BookShopSystem.Services.Data.Interfaces;
+using BookShopSystem.Web.Infrastructure.Extensions;
+using BookShopSystem.Web.ViewModels.Book;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
+using static BookShopSystem.Common.NotificationMessagesConstants;
 
 namespace BookShopSystem.Web.Controllers
 {
@@ -8,9 +12,13 @@ namespace BookShopSystem.Web.Controllers
     public class BookController : Controller
     {
         private readonly IBookService bookService;
-        public BookController(IBookService bookService)
+        private readonly IGenreService genreService;
+        private readonly IManagerService managerService;
+        public BookController(IBookService bookService, IGenreService genreService, IManagerService managerService)
         {
-            this.bookService = bookService;         
+            this.bookService = bookService;  
+            this.genreService = genreService;
+            this.managerService = managerService;   
         }
 
         /*[HttpGet]
@@ -22,8 +30,19 @@ namespace BookShopSystem.Web.Controllers
         }*/
         [HttpGet]
         public async Task<IActionResult> Add()
-        { 
+        {
+            bool isManager = await this.managerService.ManagerExistsByUserIdAsync(this.User.GetId());
+            if (!isManager)
+            {
+                this.TempData[ErrorMessage] = "You must become an manager in order to add new books!";
+                return this.RedirectToAction("Become", "Manager");
+            }
 
+            BookFormModel formModel = new BookFormModel()
+            {
+                Genries = await genreService.AllGenriesAsync()
+            };
+            return View(formModel);
         }
     }
 }
