0a345ea72447f29ce708a311cac35ef6d5624308
AleksManolow <amanolov7@gmail.com>
2023-07-23 15:57:50 +0300

    Add StatisticService

---
+++ BookShopSystem.Services.Data/Interfaces/IStatisticService.cs
@@ -0,0 +1,9 @@
+﻿using BookShopSystem.Services.Data.Models.Statistics;
+
+namespace BookShopSystem.Services.Data.Interfaces
+{
+    public interface IStatisticService
+    {
+        Task<StatisticsServiceModel> TotalAsync();
+    }
+}
---
+++ BookShopSystem.Services.Data/StatisticService.cs
@@ -0,0 +1,31 @@
+﻿using BookShopSystem.Data;
+using BookShopSystem.Services.Data.Interfaces;
+using BookShopSystem.Services.Data.Models.Statistics;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShopSystem.Services.Data
+{
+    public class StatisticService : IStatisticService
+    {
+        public readonly BookShopDbContext bookShopDbContext;
+        public StatisticService(BookShopDbContext bookShopDbContext)
+        {
+            this.bookShopDbContext = bookShopDbContext;
+        }
+        public async Task<StatisticsServiceModel> TotalAsync()
+        {
+            return new StatisticsServiceModel
+            {
+                TotalPurchases = await bookShopDbContext.Purchases.CountAsync(),
+                TotalFantasyPurchases = await bookShopDbContext.Purchases.CountAsync(p => p.Book.Genre.Name == "Fantasy"),
+                TotalSciFiPurchases = await bookShopDbContext.Purchases.CountAsync(p => p.Book.Genre.Name == "SciFi"),
+                TotalMysteryPurchases = await bookShopDbContext.Purchases.CountAsync(p => p.Book.Genre.Name == "Mystery"),
+                TotalThrillerPurchases = await bookShopDbContext.Purchases.CountAsync(p => p.Book.Genre.Name == "Thriller"),
+                TotalRomancePurchases = await bookShopDbContext.Purchases.CountAsync(p => p.Book.Genre.Name == "Romance"),
+                TotalWesternsPurchases = await bookShopDbContext.Purchases.CountAsync(p => p.Book.Genre.Name == "Westerns"),
+                TotalDystopianPurchases = await bookShopDbContext.Purchases.CountAsync(p => p.Book.Genre.Name == "Dystopian"),
+                TotalContemporaryPurchases = await bookShopDbContext.Purchases.CountAsync(p => p.Book.Genre.Name == "Contemporary"),
+            };
+        }
+    }
+}
