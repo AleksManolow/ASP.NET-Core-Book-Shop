d8b9cbf5043da410e106d2d9907aa770da339631
AleksManolow <amanolov7@gmail.com>
2023-07-23 15:59:19 +0300

    Creating pie chart

---
+++ BookShopSystem.Services.Data.Models/Statistics/StatisticsServiceModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BookShopSystem.Services.Data.Models.Statistics
+{
+    public class StatisticsServiceModel
+    {
+        public int TotalPurchases { get; set; }
+        public int TotalFantasyPurchases { get; set; }
+        public int TotalSciFiPurchases { get; set; }
+        public int TotalMysteryPurchases { get; set; }
+        public int TotalThrillerPurchases { get; set; }
+        public int TotalRomancePurchases { get; set; }
+        public int TotalWesternsPurchases { get; set; }
+        public int TotalDystopianPurchases { get; set; }
+        public int TotalContemporaryPurchases { get; set; }
+    }
+}
