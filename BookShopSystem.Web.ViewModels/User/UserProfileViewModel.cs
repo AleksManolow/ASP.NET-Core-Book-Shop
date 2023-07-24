f6ae63e0e63205b7af9840e858f0d9697cfc04d9
AleksManolow <amanolov7@gmail.com>
2023-07-24 16:18:47 +0300

    Creating UserProfileViewModel

---
+++ BookShopSystem.Web.ViewModels/User/UserProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BookShopSystem.Web.ViewModels.User
+{
+    public class UserProfileViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public decimal Wallet { get; set; }
+    }
+}
