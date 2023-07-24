a28dfbbe5b3400b8a9f94b5cdff2f9cda1054731
AleksManolow <amanolov7@gmail.com>
2023-07-24 16:15:38 +0300

    Creating UserService

---
+++ BookShopSystem.Services.Data/UserService.cs
@@ -0,0 +1,19 @@
+﻿using BookShopSystem.Data;
+using BookShopSystem.Services.Data.Interfaces;
+
+namespace BookShopSystem.Services.Data
+{
+    public class UserService:IUserService
+    {
+        public readonly BookShopDbContext dbContext;
+        public UserService(BookShopDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<UserProfileViewModel> GetUserProfileInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
