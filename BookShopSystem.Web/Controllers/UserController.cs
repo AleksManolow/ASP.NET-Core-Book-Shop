e4f921043fabb2608e01f0c1ccc5bcb1b90959ba
AleksManolow <amanolov7@gmail.com>
2023-07-24 16:38:33 +0300

    Creating User Controller

---
+++ BookShopSystem.Web/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using BookShopSystem.Services.Data.Interfaces;
+using BookShopSystem.Web.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopSystem.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        public readonly IUserService userService;
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<IActionResult> Profile() 
+        {
+            /*if ()
+            {
+
+            }*/
+            return View(await userService.GetUserProfileInfo(this.User.GetId()));  
+        }
+    }
+}
